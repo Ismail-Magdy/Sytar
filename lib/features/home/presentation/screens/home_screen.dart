9c74c685eccb4e3999213bf23c1c30b20dcb0b38
Ismail Magdy <ismailmagdy920@gmail.com>
2026-07-10 22:44:44 +0300

    Refactor code structure for improved readability and maintainability

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,13 +1,11 @@
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 import "package:flutter_screenutil/flutter_screenutil.dart";
-import "package:sytar/core/widgets/error_screen.dart"; // Adjust path
-import "package:sytar/core/helpers/spacing.dart"; // Adjust path
+import "package:sytar/core/widgets/error_screen.dart";
+import "package:sytar/core/helpers/spacing.dart";
 import "package:sytar/features/home/data/models/home_dashboard_model.dart";
 import "package:sytar/features/home/manager/home_cubit.dart";
 import "package:sytar/features/home/manager/home_state.dart";
-
-// Import all custom widgets
 import "../widgets/home_empty_state.dart";
 import "../widgets/home_greeting_widget.dart";
 import "../widgets/gpa_summary_card.dart";
@@ -20,14 +18,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      // Floating action button for quick additions
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // TODO: Open bottom sheet to add task/subject
-        },
-        backgroundColor: Colors.blueAccent,
-        child: const Icon(Icons.add, color: Colors.white),
-      ),
+      //
       body: SafeArea(
         child: BlocBuilder<HomeCubit, HomeState>(
           builder: (context, state) {
@@ -39,7 +30,6 @@ class HomeScreen extends StatelessWidget {
               final data = state.dashboardData;
               final bool isDataEmpty =
                   data.upcomingTasks.isEmpty && data.subjectsProgress.isEmpty;
-
               // Display empty state if no subjects or tasks
               if (isDataEmpty) {
                 return HomeEmptyState(userName: data.userName);
